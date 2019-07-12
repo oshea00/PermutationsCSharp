efa2f15295f4b2608dfe11eaabe49502c9072eb3
Mike OShea <oshea00@gmail.com>
2019-07-11 20:49:10 -0700

    readme

--- Program.cs
+++ Program.cs
@@ -14,24 +14,26 @@ namespace permutations
 
         ////////////////////////////////////////////////
         // A Recursive solution to permutations       //
-        // Note new tuple swap we can do now in C#!   //
+        // Note: new tuple swap we can do now in C#!   //
+        // Also, new local functionss!                //
         ////////////////////////////////////////////////
-        void PermsHelp(char[] a,int start, int end, List<string> perms) {
-            if (start == end)
-            {
-                perms.Add(new string(a));
-                return;
-            }
-            for (int i=start; i<end; i++) {
-                (a[i], a[start]) = (a[start], a[i]);
-                PermsHelp(a, start + 1, end, perms);
-                (a[i], a[start]) = (a[start], a[i]);
-            }
-        }
-
         List<string> Perms(string word) {
             var perms = new List<string>();
-            PermsHelp(word.ToArray(),0,word.Length,perms);
+
+            void PermsHelp(char[] a,int start, int end) {
+                if (start == end)
+                {
+                    perms.Add(new string(a));
+                    return;
+                }
+                for (int i=start; i<end; i++) {
+                    (a[i], a[start]) = (a[start], a[i]);
+                    PermsHelp(a, start + 1, end);
+                    (a[i], a[start]) = (a[start], a[i]);
+                }
+            }
+
+            PermsHelp(word.ToArray(),0,word.Length);
             return perms;
         }
 
